d61b3693377e6ac6c6165823830956ea261bc143
mount.dg <sondv.1597@gmail.com>
2023-11-15 10:57:36 +0700

    YS-18: Temporarily hashed sensitive data

--- yasuo-env.sh
+++ yasuo-env.sh
@@ -1,4 +1,4 @@
-export mongodbUser=yasuo-app
-export mongodbPassword=hCjbKt6Q44JP8XsG
+export mongodbUser=$2a$12$1IT5pJU5XfQogeUxMfjDRO4C3hsmD3EYJboBB2hk362YEKpmt9i92
+export mongodbPassword=$2a$12$9VsllA/pObB8SANup9HowOW5BDeOon5yIZCbbqSLjwa3W/DPvRaYa
 export mongodbUri=mongodb+srv://${mongodbUser}:${mongodbPassword}@yasuo.ebqizwv.mongodb.net/yasuo
 export sameSiteValue=None
